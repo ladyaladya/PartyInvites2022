669fd76c96468128c48bc211faf48bf30e38371b
vyshnivskyivlad <vyshnivskyivlad@gmail.com>
2022-03-17 19:34:02 +0200

    Добавьте файлы проекта.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PartyInvites.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult RSVP()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RSVP(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                GuestResponse.AddResponse(guestResponse);
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        public ViewResult Thanks()
+        {
+            return View();
+        }
+        public ViewResult GuestsList()
+        {
+            return View("GuestsList", GuestResponse.GuestResponses.Where(x => x.WillAttend == true));
+        }
+    }
+}
---
+++ Models/GuestResponse.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PartyInvites.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage = "Please enter your name.")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Please enter your email.")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Please enter your phone")]
+        public string Phone { get; set; }
+        [Required(ErrorMessage = "Please choose if you will attend party!")]
+        public bool? WillAttend { get; set; }
+        private static List<GuestResponse> _guestResponses = new List<GuestResponse>();
+        public static IEnumerable<GuestResponse> GuestResponses { 
+            get
+            {
+                return _guestResponses;
+            }
+        }
+
+        public static void AddResponse(GuestResponse response)
+        {
+            _guestResponses.Add(response);
+        }
+    }
+}
